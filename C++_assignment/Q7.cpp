13d215aaded23217ef79c00d72cc6fde262b3b77
Michael <michaelhesham20@gmail.com>
2021-04-20 14:20:50 +0200

    c++_assignment_modification_Q7

--- C++_assignment/Q7.cpp
+++ C++_assignment/Q7.cpp
@@ -4,6 +4,12 @@ using namespace std;
 class AddComplex{
     public:
         int real, img;
+        void add_real(AddComplex x){
+            cout << "sum of real parts is "<< real+x.real<<endl;
+        }
+        void add_img(AddComplex x){
+            cout << "sum of img parts is "<< img+x.img<<endl;
+        }
 };
 int main(){
     AddComplex first,second;
@@ -15,7 +21,8 @@ int main(){
     cin >> second.real;
     cout << "Enter the imaginary part: ";
     cin >> second.img;
-    cout << "sum of real parts is "<< first.real+second.real<<endl;
-    cout << "sum of img parts is "<< first.img+second.img<<endl;
+    first.add_real(second);
+    first.add_img(second);
+    
     return 1;
 }
