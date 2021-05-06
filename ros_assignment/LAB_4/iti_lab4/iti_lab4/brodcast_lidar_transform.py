861dc600e70f9abae7fcce60ebdb54c6026fde37
Michael <michaelhesham20@gmail.com>
2021-05-06 14:58:27 +0200

    LAB4_ROS

---
+++ ros_assignment/LAB_4/iti_lab4/iti_lab4/brodcast_lidar_transform.py
@@ -0,0 +1,83 @@
+from math import sin, cos, pi
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from geometry_msgs.msg import Quaternion
+from sensor_msgs.msg import JointState
+from tf2_ros import TransformBroadcaster, TransformStamped
+
+class StatePublisher(Node):
+
+    def __init__(self):
+        rclpy.init()
+        super().__init__('brodcast_lidar_transform')
+
+        qos_profile = QoSProfile(depth=10)
+        self.joint_pub = self.create_publisher(JointState, 'joint_states', qos_profile)
+        self.broadcaster = TransformBroadcaster(self, qos=qos_profile)
+        self.nodeName = self.get_name()
+        self.get_logger().info("{0} started".format(self.nodeName))
+
+        degree = pi / 180.0
+        loop_rate = self.create_rate(30)
+
+        # robot state
+        tilt = 0.
+        swivel = 0.
+        angle = -pi/2
+        height = 0.
+        flag = 0
+        # message declarations
+        lidar_trans = TransformStamped()
+        lidar_trans.header.frame_id = 'servo_link'
+        lidar_trans.child_frame_id = 'lidar_link'
+        joint_state = JointState()
+
+        try:
+            while rclpy.ok():
+                rclpy.spin_once(self)
+
+                # update joint_state
+                now = self.get_clock().now()
+                joint_state.header.stamp = now.to_msg()
+                joint_state.name = ['swivel', 'tilt', 'periscope']
+                joint_state.position = [swivel, tilt, height]
+
+                # update transform
+                # (moving in a circle with radius=2)
+                lidar_trans.header.stamp = now.to_msg()
+                lidar_trans.transform.rotation = \
+                    euler_to_quaternion(0, angle + pi/2, 0) # roll,pitch,yaw
+
+                # send the joint state and transform
+                self.joint_pub.publish(joint_state)
+                self.broadcaster.sendTransform(lidar_trans)
+
+                # Create new robot state
+                if angle < -30 * (pi/180) and flag == 0:
+                    angle += degree/4
+                elif angle > -35 * (pi/180):
+                    angle -= degree/4
+                    flag = 1
+                print(30 * (pi/180))
+                print(angle)
+
+		
+                # This will adjust as needed per iteration
+                loop_rate.sleep()
+
+        except KeyboardInterrupt:
+            pass
+
+def euler_to_quaternion(roll, pitch, yaw):
+    qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2)
+    qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2)
+    qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2)
+    qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2)
+    return Quaternion(x=qx, y=qy, z=qz, w=qw)
+
+def main():
+    node = StatePublisher()
+
+if __name__ == '__main__':
+    main()
